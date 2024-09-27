f706ff62a2fd27d18ddd1356d2c227f5928679d4
Gabriel Souza <gabrielmaestasouza@gmail.com>
2024-09-27 16:18:57 -0300

    Scenario 2 case 2 created

---
+++ cypress/e2e/Scenario2/Case2.cy.js
@@ -0,0 +1,54 @@
+describe('API Test Scenario 2 Case 1 - Reading product description', () => {
+
+    it('Register a product and verify that it was added correctly', () => {
+      const apiUrl = Cypress.env('apiUrl'); 
+      
+
+
+      const produto = {
+        title: "Produto A",
+        price: 10,
+        categoryId: 1,
+        images: ["https://placeimg.com/640/480/any"]
+      };
+  
+      
+      cy.request({
+        method: 'POST',
+        url: `${apiUrl}`, 
+        body: produto
+      }).then((response) => {
+        
+        expect(response.status).to.eq(201);
+  
+        expect(response.body.title).to.eq(produto.title);
+        expect(response.body.price).to.eq(produto.price);
+        expect(response.body.description).to.eq(produto.description);
+  
+        const productId = response.body.id;
+  
+        // Search for new product in product list from Id
+        cy.request({
+          method: 'GET',
+          url: `${apiUrl}`, 
+        }).then((getResponse) => {
+          
+          expect(getResponse.status).to.eq(200);
+  
+          const produtoCadastrado = getResponse.body.find(p => p.id === productId);
+  
+          expect(produtoCadastrado).to.not.be.undefined;
+  
+          expect(produtoCadastrado.title).to.eq(produto.title);
+          expect(produtoCadastrado.price).to.eq(produto.price);
+          if (expect(produtoCadastrado.description).to.eq(produto.description));
+          {     
+            cy.log("Detalhes do produto A: " + produtoCadastrado.description);
+            cy.log("Descrição visualizada com sucesso!");
+          };
+        });
+      });
+    });
+  
+  });
+  
